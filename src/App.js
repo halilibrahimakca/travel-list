d3ff05223fc1c4536b3023cbb9134d181fb04c86
halilibrahimakca <halilibrahimakcaa@gmail.com>
2023-08-14 16:16:30 +0300

    feat(src): add statistics to footer in App.js

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -103,10 +103,24 @@ function Item({ item, onDeleteItem, onToggleItem }) {
     </li>
   );
 }
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>👜 You have X items on yout list, and you already packed X (X%)</em>
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go ✈️"
+          : `👜 You have ${numItems} items on yout list, and you already packed ${numPacked} (${percentage}%)`}
+      </em>
     </footer>
   );
 }
